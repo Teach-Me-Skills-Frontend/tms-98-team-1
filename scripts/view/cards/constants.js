a349c2a2f80b39c16c7bbc20df0f401cc6f51905
wdawadwda <wdawadwdawork@gmail.com>
2023-03-15 23:02:48 +0300

    feat:
    add utils funk createItem createEl, folder constsnts with class Name
    
    problem with svg
    array of cards is empty

---
+++ scripts/view/cards/constants.js
@@ -0,0 +1,14 @@
+export const container = document.querySelector('.s-cards__item-wrapper')
+export const cardClassName = {
+    itemKey: 's-cards__item-wrapper__item',
+    imgKey: 's-cards__item-wrapper__item-img',
+    wrapperTextKey: 's-cards__item-wrapper__item__wrapper-text',
+    priceKey: 's-cards__item-wrapper__item__wrapper-text__price cards-text',
+    deliveryKey: 's-cards__item-wrapper__item__wrapper-text__delivery-time cards-text',
+    nameKey: 's-cards__item-wrapper__item__wrapper-text__name cards-text',
+    aKey: 's-cards__item-wrapper__item__wrapper-text__svg-wrapper',
+    // svgKey: 's-cards__item-wrapper__item__wrapper-text__svg-wrapper__svg',
+    svgKey: 'fa-solid fa-star',
+    rateKey: 's-cards__item-wrapper__item__wrapper-text__svg-wrapper__rate cards-text',
+    btnKey: 's-cards__item-wrapper__item__btn btn'
+}
