fb332f9e9e195e3f22dbc12b8cd3a0ee401c5f8a
wdawadwda <wdawadwdawork@gmail.com>
2023-03-17 13:28:52 +0300

    fix: made changes according to the review

--- scripts/view/slider/constants.js
+++ scripts/view/slider/constants.js
@@ -1,2 +1,9 @@
 export const images = document.querySelectorAll('#sliderLine img');
-export const sliderLine = document.querySelector('#sliderLine')
+export const sliderLine = document.querySelector('#sliderLine');
+export const slider = document.querySelector('#slider');
+export const idSlider = {
+    sliderPrev: 'slider_prev',
+    sliderPrev1: 'btn_prev',
+    sliderNext:'slider_next',
+    sliderNext1:'btn_next'
+}
