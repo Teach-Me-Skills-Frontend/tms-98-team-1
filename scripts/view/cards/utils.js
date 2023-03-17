07e22f4aa8d2c4f4bc5954db4433f427e1bf0ddc
wdawadwda <wdawadwdawork@gmail.com>
2023-03-17 13:28:52 +0300

    fix: made changes according to the review

--- scripts/view/cards/utils.js
+++ scripts/view/cards/utils.js
@@ -1,12 +1,10 @@
-import {container} from './constants.js';
 import { cardClassName as clName } from './constants.js';
+
 const {itemKey, imgKey, wrapperTextKey, priceKey, deliveryKey, nameKey, aKey, svgKey, rateKey, btnKey} = clName;
 
 export function createItem ({img, cardName, price, cardRate, id}) {
-    const wrapper = container;
 
     const item = createEl('div',{class: itemKey, id: `${id}`});
-    wrapper.append(item);
     
     const imgEl = createEl('img',{class: imgKey, src: `${img}`});
     item.append(imgEl);
@@ -29,13 +27,6 @@ export function createItem ({img, cardName, price, cardRate, id}) {
     const svg = createEl('i',{class: svgKey});
     a.append(svg);
 
-    // const svg = createEl('svg',{class: svgKey});
-    // a.append(svg);
-
-    // const use = createEl('use'); //todo Свг из спрайта не работает
-    // use.setAttribute('xlink:href', '#star')
-    // svg.append(use);
-
     const rate = createEl('div',{class: rateKey}, `${cardRate}`);
     a.append(rate);
 
@@ -43,24 +34,22 @@ export function createItem ({img, cardName, price, cardRate, id}) {
     button.setAttribute('data-row-btn', 'бла-бла-бла')
     wrapperText.append(button);
 
-    return wrapper;
+    return item;
 }
 
 export function createEmptyItem(){
-    const wrapper = container;
-    const item = createEl('div',{}, 'Что-то пошло не так, товаров нет');
-    wrapper.append(item);
-    return wrapper
+    const emptyItem = createEl('div',{}, 'Что-то пошло не так, товаров нет');
+    return emptyItem
 }
 
 // Функции создания элементов
 //_________________________________________
 
 function createEl(tag, elProps, text = ''){
-    const variable = document.createElement(tag)
-    variable.innerHTML = text
+    const el = document.createElement(tag)
+    el.textContent = text
     for (const key in elProps) {
-        variable.setAttribute(key, elProps[key]);
+        el.setAttribute(key, elProps[key]);
     }
-    return variable;
+    return el;
 }
