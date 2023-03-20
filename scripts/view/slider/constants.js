12c906177d0cf1b366cf61dd9631e54e61ded78f
wdawadwda <wdawadwdawork@gmail.com>
2023-03-20 19:19:07 +0300

    adaptive

--- scripts/view/slider/constants.js
+++ scripts/view/slider/constants.js
@@ -3,5 +3,7 @@ export const sliderLine = document.querySelector('#sliderLine');
 export const slider = document.querySelector('#slider');
 export const idSlider = {
     sliderPrev: 'slider_arrow__left',
+    sliderPrev1: 'slider_prev',
     sliderNext:'slider_arrow__right',
+    sliderNext1:'slider_next',
 }
